63c08105a32d17b3608df413a52f940b1af6754b
ZaidHanania <zaid.hanania@gmail.com>
2018-01-13 03:43:54 +0200

    Fixed more android dependencies

--- android/app/src/main/java/com/contacts/MainApplication.java
+++ android/app/src/main/java/com/contacts/MainApplication.java
@@ -3,7 +3,6 @@ package com.contacts;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
-import com.oblador.vectoricons.VectorIconsPackage;
 import com.react.rnspinkit.RNSpinkitPackage;
 import com.rt2zz.reactnativecontacts.ReactNativeContacts;
 import com.facebook.react.ReactNativeHost;
@@ -26,7 +25,6 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
-            new VectorIconsPackage(),
             new RNSpinkitPackage(),
             new ReactNativeContacts()
       );
